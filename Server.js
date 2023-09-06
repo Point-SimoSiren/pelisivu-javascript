a5671858613143797232e5d43e28a670c22da630
Simo Sirén <simo.siren1@careeria.fi>
2023-09-06 11:06:32 +0300

    kommentteja

--- Server.js
+++ Server.js
@@ -2,16 +2,20 @@ const Express = require('express')
 const App = Express()
 const Port = 5100
 const Path = require('path')
-// Taulukollinen mopo objekteja:
-const mopot = [{id: 1, merkki: "Keeway"},{id: 2, merkki: "Suzuki"}]
 
+//*** FRONTTI */
 // Tehdään polkumääritys public kansioon 
 const Polku = Path.join(__dirname, './public')
-
+// Tämä toiminto tarjoaa html, css, js tiedostot
+// eli selaimeen web käyttöliittymän
 App.use(Express.static(Polku))
 
+
+//*** BACKEND REST API */
 // api/mopot polkuun tulevaan pyyntöön vastataan
-// lähettämällä mopo data json muodossa.
+// lähettämällä mopo dataa json muodossa.
+const mopot = [{id: 1, merkki: "Keeway"},{id: 2, merkki: "Suzuki"}]
+
 App.get('/api/mopot', (req, res) => {
     res.json(mopot)
 })
